43f6a9a5363dbffacdc11bfb9fc2aa068d69c2e6
MacKaiver <mackaiver@freenet.de>
2014-10-26 20:03:59 +0100

    Aufgabe 4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-SMD
+SMDd
 ===
 
 Übungsabgaben für SMD
--- blatt_1/blatt_1.py
+++ blatt_1/blatt_1.py
@@ -1,6 +1,8 @@
 __author__ = 'awesome bitches'
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
 import random
 import math
 
@@ -22,8 +24,37 @@ def aufgabe_3():
 
 
 def aufgabe_4():
-    pass
+    steps = 50
 
+    xs = np.linspace(0.9, 1.1, num=steps)
+
+    a = lambda x: (1-x)**6
+    b = lambda x: x**6 - 6*(x**5) + 15*x**4 - 20*x**3 + 15*x**2 - 6*x + 1
+    c = lambda x: x*(x*(x*(x*((x - 6)*x + 15) - 20) + 15) - 6) + 1
+
+    fig, (top, bottom) = plt.subplots(nrows=2, ncols=1)
+    top.plot(xs, a(xs), color='red', label="Direkte Auswertung")
+    top.plot(xs, b(xs), color='blue', label="Ausmultiplizieren")
+    top.plot(xs, c(xs), color='green', label="Horner Schema")
+    top.set_ylabel('$f(x) = (1-x)^6$')
+    top.set_xlabel('$x$')
+
+    #jetzt auf kleinerem bereich
+    xs = np.linspace(0.9990001, 1.001, num=steps)
+    bottom.plot(xs, a(xs), color='red')
+    bottom.plot(xs, b(xs), color='blue')
+    bottom.plot(xs, c(xs), color='green')
+    bottom.set_ylabel('$f(x) = (1-x)^6$')
+    bottom.set_xlabel('$x$')
+
+    #style für die label
+    top.ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
+    bottom.get_yaxis().set_label_coords(-0.1, 0.5)
+    top.get_yaxis().set_label_coords(-0.1, 0.5)
+    handles, labels = top.get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor=[1.01, 0.97], ncol=1, borderaxespad=1.25)
+    fig.suptitle('Polynom Auswertungen', fontsize=14, fontweight='bold')
+    #top.title("my title", y=0.6)
 
 
 def main():
@@ -33,6 +64,11 @@ def main():
     aufgabe_2()
     print("\nAufgabe 3")
     aufgabe_3()
+    print("\nAufgabe 4")
+    aufgabe_4()
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
