adcd86e217cf2cbb3b552a1a87324977e6c79606
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2014-11-23 19:37:31 +0100

    Versagen auf ganzer Linie

--- blatt_5/aufgabe_1.py
+++ blatt_5/aufgabe_1.py
@@ -18,7 +18,11 @@ def main():
     x = np.linspace(-5, 10, 50)
     y = np.linspace(-5, 10, 50)
     X, Y = np.meshgrid(x, y)
-    Z = normal(X, Y, 4, 2, 3.5, 1.5)
+    sigma_x = 3.5
+    sigma_y = 3.4
+    cor = 0.8
+    covariance = 0.1
+    Z = normal(X, Y, 4, 2, sigma_x, sigma_y)
 
     fig, ax = plt.subplots(nrows=1, ncols=1)
     t = ax.pcolormesh(X, Y, Z, shading='gouraud')
@@ -27,27 +31,34 @@ def main():
     # d =  np.random.multivariate_normal(mean, cov, N)
     # h, ye, xe, misc = ax.hist2d(d[:,0], d[:,1], bins=100)
 
-    angle = 20
-    e = Ellipse(xy=(4, 2), width=3.5, height=1.5, angle=angle)
+
+    angle = 0.5 * np.arctan(2* cor * sigma_x * sigma_y/(sigma_x**2 - sigma_y**2))
+    cov = [[sigma_x**2, covariance], [covariance, sigma_y**2]]
+    rot = [[np.cos(angle), np.sin(angle) ], [-np.sin(angle), np.cos(angle)]]
+
+    matrix = np.dot(np.transpose(rot), np.dot(cov, rot))
+
+    print("MATRIX: ", matrix)
+
+    w, v = np.linalg.eig(cov)
+    matrix = np.dot(np.transpose(v), np.dot(cov, v))
+
+    print("NEUE AMTRIDJFSDAM: ", matrix)
+
+
+
+    e = Ellipse(xy=(4, 2), width=matrix[0,0], height= matrix[1,1], angle= np.rad2deg(angle))
     e.set_color("white")
     e.set_alpha(0.8)
     ax.add_artist(e)
 
     #draw a line
     x= [4,10]
-    y= [2, np.arctan(angle)*4]
+    y= [2, 2 + np.tan(angle)*6]
     plt.plot(x, y, color="white", linewidth=2)
     plt.show()
 
-#     from pylab import *
-#
-#
-# Z = (1 - X/2. + X**5 + Y**3) * exp(-X**2 - Y**2) # calcul du tableau des valeurs de Z
-#
-# pcolormesh(X, Y, Z, shading='gouraud')
-# colorbar()
-#
-# show()
+
 
 
 if __name__ == '__main__':
---
+++ blatt_5/aufgabe_2.py
@@ -0,0 +1,65 @@
+__author__ = 'Kai'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib.patches import Ellipse
+
+def normal(N = 10000):
+    l = np.zeros((N,2))
+    for i in range(N):
+        mu_x = 4
+        sigma_x = 1.5
+        x = np.random.normal(mu_x, sigma_x, 1)
+        mean = 2+0.35*(x-4)
+        sigma = 0.9
+        y =np.random.normal(mean, sigma, 1)
+        l[i,0] = x
+        l[i,1] = y
+    return l
+def modi(b):
+    mean_b = (b[:,0].mean(), b[:,1].mean())
+    sigma_b = (b[:,0].std(), b[:,1].std())
+    cov_b = np.cov(b[:,0], b[:, 1])
+    cor = cov_b[0,1]/(sigma_b[0]* sigma_b[1])
+    return mean_b, sigma_b, cov_b, cor
+
+def main():
+
+    fig, (top, center, bottom) = plt.subplots(nrows=3, ncols=1)
+    #fig.colorbar(t, ax=ax)
+    mean = [0,4]
+    cov = [[1.5**2, 0], [0, 1.5**2]]
+    N = 50000
+
+    a = np.random.multivariate_normal(mean, cov, N)
+    print("Mean, sigma and covariance for distribution A: ", modi(a))
+
+
+    b = normal(N=N)
+    print("Mean, sigma and covariance for distribution B: ", modi(b))
+
+
+    c = np.append(a, b, axis=0)
+    print("Mean, sigma and covariance for total distribution: ", modi(c))
+
+
+
+    print(c)
+    h, ye, xe, misc = top.hist2d(a[:,0], a[:,1], bins=40)
+    fig.colorbar(misc, ax=top)
+    h, ye, xe, misc = center.hist2d(b[:,0], b[:,1], bins=40)
+    fig.colorbar(misc, ax=center)
+    h, ye, xe, misc = bottom.hist2d(c[:,0], c[:,1], bins=40)
+    fig.colorbar(misc, ax=bottom)
+
+
+
+
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    main()
